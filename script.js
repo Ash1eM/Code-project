60820c062766c0e3989db3a2d7c6064ca15eb04b
Ash1eM <ashley.morris101@yahoo.com>
2023-02-05 20:16:21 -0500

    ?

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ var firstName;
 firstName = "Ashley";  
 var lastName;
 lastName = "Morris";
+var firstNameLastName;
+var firstNameLastName = "Ashley Morris";
 var age;
-age = 20;
-alert(firstName + lastName);
+age = 20; 
+alert("My name is" + firstNameLastName); 
+alert("My age is" + age);
 console.log("I am having so much faun and learning a lot. Thank you profezzor")
