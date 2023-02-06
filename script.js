ce5599a2964f03c669c05f66748c5c0a57ff3676
Ash1eM <ashley.morris101@yahoo.com>
2023-02-05 21:08:30 -0500

    modified

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 var firstName;
-firstName = "Ashley";  
+firstName = "Ashley ";  
 var lastName;
-lastName = "Morris";
-var firstNameLastName;
-var firstNameLastName = "Ashley Morris";
+lastName = "Morris ";
 var age;
-age = 20; 
-alert("My name is" + firstNameLastName); 
-alert("My age is" + age);
+age = 20;
+alert("My name is " + "" + firstName + "" + lastName); 
+alert("My age is " + age);
 console.log("I am having so much faun and learning a lot. Thank you profezzor")
